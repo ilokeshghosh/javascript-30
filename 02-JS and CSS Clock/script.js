bd7664a06ededa6b7fb92e3dd86077090a565962
Lokesh Ghosh <lghosh857@gmail.com>
2023-09-13 19:08:11 +0530

    day 2 : js and css Clock
    Analog clock using html , css and js

---
+++ 02-JS and CSS Clock/script.js
@@ -0,0 +1,41 @@
+const secondHand = document.querySelector('.second-hand')
+const minHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
+function setDate() {
+    const now = new Date();
+
+    // second
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    // const secondsDegrees = ((seconds / 60) * 360) + 90;
+    // console.log(secondsDegrees);
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+
+    // minute
+    const minutes = now.getMinutes();
+    const minsDegrees = ((minutes / 60) * 360) + 90;
+    // const minsDegrees = ((minutes + seconds / 60) / 60) * 360 + 90;
+    // const minsDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+
+
+
+    // hours
+    const hours = now.getHours();
+    // const hourDegree = ((hours / 12) * 360) + 90;
+    const hourDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
+    // console.log(hourDegree)
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+
+
+
+
+
+
+}
+
+setInterval(setDate, 1000)
+setDate();
