bc8dfd2bffdc6be37fe822ee8998b3f22c4c997b
Lokesh Ghosh <lghosh857@gmail.com>
2023-09-14 08:35:51 +0530

    Update script.js

--- 02-JS and CSS Clock/script.js
+++ 02-JS and CSS Clock/script.js
@@ -15,8 +15,6 @@ function setDate() {
     // minute
     const minutes = now.getMinutes();
     const minsDegrees = ((minutes / 60) * 360) + 90;
-    // const minsDegrees = ((minutes + seconds / 60) / 60) * 360 + 90;
-    // const minsDegrees = ((minutes / 60) * 360) + ((seconds/60)*6) + 90;
     minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
 
@@ -24,8 +22,8 @@ function setDate() {
 
     // hours
     const hours = now.getHours();
-    // const hourDegree = ((hours / 12) * 360) + 90;
-    const hourDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
+    const hourDegrees = ((hours / 12) * 360) + 90;
+    // const hourDegrees = ((hours / 12) * 360) + ((minutes/60)*30) + 90;
     // console.log(hourDegree)
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
